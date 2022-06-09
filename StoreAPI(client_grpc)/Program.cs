7981d33a584fdd20120f501c273c675ae94b55b7
RehabHesham <52565633+RehabHesham@users.noreply.github.com>
2022-06-09 21:18:41 +0200

    Revert "angularClient"
    This reverts commit 3600cd9f082375b167b0c78be34261694b084f63.

--- StoreAPI(client_grpc)/Program.cs
+++ StoreAPI(client_grpc)/Program.cs
@@ -1,22 +1,11 @@
-using Google.Protobuf.WellKnownTypes;
-using Microsoft.AspNetCore.Authentication;
-using StoreAPI_client_grpc_;
-using StoreAPI_client_grpc_.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//const string schema = "BasicAuthentication"; 
-//builder.Services.AddAuthentication(schema)
-//    .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>(schema, opt =>
-//    {
 
-//    });
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -27,16 +16,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseCors(builder => builder
-     .AllowAnyOrigin()
-     .AllowAnyMethod()
-     .AllowAnyHeader());
-
 app.UseHttpsRedirection();
-
-app.UseGrpcWeb(new GrpcWebOptions { DefaultEnabled = true });
-
-app.MapGrpcService<gRPC_OrdreService>().RequireCors("All");
 
 app.UseAuthorization();
 
